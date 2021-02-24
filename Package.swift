6eedc5238ce3dc28b8ebc6cd6ec6a50f196d5c51
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2021-02-24 15:48:17 -0800

    Update

--- Package.swift
+++ Package.swift
@@ -12,15 +12,15 @@ let package = Package(
             targets: ["PDFTron.Tools"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/JamieDass/PTSPMTest", Package.Dependency.Requirement)
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "PDFTron.Tools",
-            dependencies: []),
-        .testTarget(
-            name: "PDFTron.ToolsTests",
-            dependencies: ["PDFTron.Tools"]),
+        .binaryTarget(
+            name: "Tools",
+            url: "https://pdftron.s3.amazonaws.com/custom/ID-zJWLuhTffd3c/iOS/SPM/Tools.xcframework.zip",
+            checksum: "7c8af303ecf914dab3db1a58e12355e0c3332d05ec79954a9321e731695f363a"
+        )
     ]
 )
