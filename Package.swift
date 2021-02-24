0d18bf4ddcb65bc8434554558eb08a1b952d1884
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2021-02-24 15:54:12 -0800

    Update target

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "Tools",
+            name: "PDFTron.Tools",
             url: "https://pdftron.s3.amazonaws.com/custom/ID-zJWLuhTffd3c/iOS/SPM/Tools.xcframework.zip",
             checksum: "7c8af303ecf914dab3db1a58e12355e0c3332d05ec79954a9321e731695f363a"
         )
